448e8a06340643e26fc5a05e0b5268431271ad9c
twierzchowski <twierzchowski@future-processing.com>
2014-07-28 13:26:22 +0200

    'fix' for cipher method

--- src/main/java/UsefulMethods.java
+++ src/main/java/UsefulMethods.java
@@ -9,15 +9,19 @@ public class UsefulMethods {
         return fact;
     }
 
-    public static String cipher(String msg, int shift){
+    public static String cipher(String msg, int shift) {
         String s = "";
     	int len = msg.length();
-	for(int x = 0; x < len; x++){
-            char c = (char)(msg.charAt(x) + shift);
-        	if (c > 'z')
-                    s += (char)(msg.charAt(x) - (26-shift));
-	        else
-        	    s += (char)(msg.charAt(x) + shift);
+	for(int x = 0; x < len; x++){ 
+	    char c = msg.charAt(x);
+	    if(c>='a' && c<='z') {
+		s += (char)(((msg.charAt(x) - 'a' + (shift%26)+26)%26)+'a');
+	    }
+	    else if (c>='A' && c<='Z') {
+		s += (char)(((msg.charAt(x) - 'A' + (shift%26)+26)%26)+'A');
+	    }
+	    else
+		return "";
         }
     return s;
     }
